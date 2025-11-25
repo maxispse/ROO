ab49b7533138a8bd6cd3f92b913436572f2059a9
maxispse <165880370+maxispse@users.noreply.github.com>
2025-11-25 13:34:28 +0100

    laravel?

---
+++ vite-project/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue';
+
+const username = ref('');
+function Hello() {
+  alert(`Your username is ${username.value}`);
+}
+</script>
+
+<template>
+  <div>
+    <h1>Hello, {{ username }}</h1>
+    <input v-model="username"/>
+    <button @click="Hello">
+      Alert
+    </button>
+  </div>
+</template>
+
